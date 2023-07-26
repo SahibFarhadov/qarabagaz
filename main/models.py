0b79f100e146e48e5d3bc189236d6aa32bc47fd7
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-07-27 02:01:56 +0400

    Navbar elave edildi. Person modeli yaradildi. Rutbe modeli yaradildi ancaq bosdur. Migrate edilib.

--- main/models.py
+++ main/models.py
@@ -3,3 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
 	pass
+
+class Person(models.Model):
+	# Şəxsi məlumatlar --------------
+	ad_soyad=models.CharField(verbose_name="Ad və soyad",max_length=255)
+	leqeb=models.CharField(verbose_name="Ləqəbi",max_length=100,default="yoxdur.",blank=True)
+	dogum_tarixi=models.DateField(verbose_name="Doğum tarixi")
+	vefat_tarixi=models.DateField(verbose_name="Vəfat tarixi")
+	dogum_yeri=models.CharField(verbose_name="Doğulduğu yer",max_length=255)
+	vefat_yeri=models.CharField(verbose_name="Vəfat yeri", max_length=255)
+	vetendasligi=models.CharField(verbose_name="Vətəndaşlığı",max_length=50)
+	
+	# Hərbi məlumatlar --------------
+	qosun_novu=models.CharField(verbose_name="Qoşun növü",max_length=100)
+	mensubiyyeti=models.CharField(verbose_name="Mənsubiyyəti",max_length=100)
+	xidmet_illeri=models.CharField(verbose_name="Xidmət illəri",max_length=100)
+
+
+class Rutbe(models.Model):
+	pass
